0da28bd5e4735029838da137f5869d93241a20a9
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-07-26 23:16:48 +0530

    fixed responsiveness

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,9 +1,9 @@
 export default function Heading({ text }) {
   return (
-    <div className="flex items-center justify-center w-1/2 mx-auto">
-      <div className="flex-grow h-px bg-black"></div>
-      <div className="mx-4 font-bold text-2xl uppercase">{text}</div>
-      <div className="flex-grow h-px bg-black w-4"></div>
+    <div className="flex items-center justify-center w-[80%] md:w-[60%] lg:w-1/2 mx-auto">
+      <div className="h-[2px] flex-grow w-4 bg-black"></div>
+      <div className="mx-4 font-bold text-base  md:text-2xl uppercase">{text}</div>
+      <div className="h-[2px] flex-grow bg-black w-4"></div>
     </div>
   );
 }
