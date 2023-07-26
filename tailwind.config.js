117db8c6bdce0ba4c52d04aec07ca8fea8a529e7
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-07-27 00:48:29 +0530

    addded mui-tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,26 @@
+// const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'mainGray': '#343538',
+        mainGray: "#343538",
       },
       backgroundColor: {
-        'mainGray': '#343538',
-      }
+        mainGray: "#343538",
+      },
     },
   },
   plugins: [],
-}
+};
